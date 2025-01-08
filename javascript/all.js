a079182ec7e52e7190ef39ef748cebe6e9a71256
akhmadzakym <136841777+akhmadzakym@users.noreply.github.com>
2025-01-08 05:58:39 +0000

    modified:   aboutme.html 	modified:   javascript/all.js

--- javascript/all.js
+++ javascript/all.js
@@ -2,7 +2,6 @@
 var elements = document.getElementsByClassName("kolom");     // Untuk grid gambar //
                                                              //                   //
 //ini gatau apaan yang diatas. 
-//DEKLARASI TITIT EH MAAF MAKSUDNYA VARIABLE 'LOOP'
 //stres gw anj beneran
 //harus pake javascript
 //kesel
@@ -22,4 +21,4 @@ if(document.documentElement.clientWidth < 900) { //katanya ini untuk viewport
                                                                                 // Biar responsif, gw berikan JS ini untuk   //
 if(screen.width < 900) { //untuk layar katanya                                  // ganti ke tampilan mobile secara otomatis. //
     window.location = "mobile/index.html"                                       //                                           //
-}                                                                               //                                           //
+}                                                                               // it's a bad practice                       //
