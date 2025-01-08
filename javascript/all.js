9ada0dc8aad21946c96a63223a4ef32138d8ea33
akhmadzakym <136841777+akhmadzakym@users.noreply.github.com>
2025-01-08 10:38:08 +0000

    modified:   CSS.css 	modified:   d_art.html 	modified:   javascript/all.js
        i regeret doing this

--- javascript/all.js
+++ javascript/all.js
@@ -23,22 +23,3 @@ if(screen.width < 900) { //untuk layar katanya                                  
     window.location = "mobile/index.html"                                       //                                           //
 }                                                                               // it's a bad practice                       //
 
-//untuk modal image
-
-var modal  = document.getElementById("myModal");
-
-var img = document.getElementsByClassName("myImg");
-var modalImg  = document.getElementById("img");
-var captionText = document.getElementById("caption");
-
-img.onclick = function(){
-    modal.style.display = "block";
-    modalImg = this.src;
-    captionText.innerHTML = this.alt;
-}
-
-var span = document.getElementsByClassName("close")[0];
-
-span.onclick = function(){
-    modal.style.display = "none";
-}
