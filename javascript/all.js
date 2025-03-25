4c57029fc2da2e813be880e0cddba79fa0496f08
akhmadzakym <136841777+akhmadzakym@users.noreply.github.com>
2025-03-25 07:07:46 +0000

    JSJSJSJSJSJSJSJSJSJSJS

--- javascript/all.js
+++ javascript/all.js
@@ -1,6 +1,6 @@
 import * as THREE from '/home/codespace/.cache/typescript/5.8/node_modules/@types/three/index.d.ts';
 
-import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
+import { OrbitControls } from '/workspaces/jakidekapi/node_modules/three/examples/jsm/controls/OrbitControls.js';
 
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
